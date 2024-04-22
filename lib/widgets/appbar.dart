49d2b177891a3af9a05f13b6c37520052230711d
dhanush-10 <126861293+dhanush-10@users.noreply.github.com>
2024-04-22 21:59:26 +0530

    initial

---
+++ lib/widgets/appbar.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppBAR extends StatelessWidget {
+  const AppBAR({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+    );
+  }
+}
